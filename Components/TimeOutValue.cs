2c2cd5f225bb9440a4fd6199f1330d12a586edd2
Tihon <science.matter@gmail.com>
2019-03-08 01:41:40 +0200

    Timeouts and SinLerp

---
+++ Components/TimeOutValue.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum TOBehaviour {
+	Refresh,
+	Persist,
+	Accumulate
+}
+
+public interface ITimeOut {
+	void Start( float duration, TOBehaviour behaviour );
+}
+
+public class TimeOut : ITimeOut {
+	private float m_start;
+	private float m_duration;
+	
+	public bool IsAvailable {
+		get { return GetAtTime( Time.time ); }
+	}
+	
+#region Public
+	public void Start( float duration, TOBehaviour behaviour ) {
+		switch( behaviour ) {
+			case TOBehaviour.Refresh:
+				_SetTimerFromNow( duration );
+				break;
+			case TOBehaviour.Persist:
+				if( IsAvailable ) {
+					_SetTimerFromNow( duration );
+				}
+				break;
+			case TOBehaviour.Accumulate:
+				if( IsAvailable ) {
+					_SetTimerFromNow( duration );
+				}
+				else {
+					m_duration += duration;
+				}
+				break;
+		}
+	}
+	
+	public bool GetAtTime( float time ) {
+		return time >= (m_start + m_duration);
+	}
+	
+	public override string ToString() {
+		return "#"+GetHashCode()+"From "+m_start.ToString( "00.00" )+" for "+m_duration.ToString( "00.00" );
+	}
+#endregion
+	
+	
+#region Private
+	private void _SetTimerFromNow( float duration ) {
+		m_start = Time.time;
+		m_duration = duration;
+	}
+#endregion
+}
---
+++ General/SinLerp.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum SinShape {
+	CRising,	// [0 -> 1] First quarter of Sin()
+	CFalling,	// [1 -> 0] Second quarter
+	JRising,	// [0 -> 1] Fourth quater + 1
+	JFalling	// [1 -> 0] Third quarter + 1
+}
+
+public static class Sin {
+	private static float _halfPI { get { return Mathf.PI *0.5f; } }
+	
+#region Implementation
+#endregion
+	
+	
+#region Public
+	public static float Lerp( float factor, SinShape shape ) {
+		return Lerp( 0, 1, factor, shape );
+	}
+	
+	public static float Lerp( float min, float max, float factor, SinShape shape ) {
+		var shift = 0f;
+		var lift = 0f;
+		switch( shape ) {
+			case SinShape.CFalling:
+				shift = _halfPI;
+				break;
+			case SinShape.JRising:
+				shift = 3 *_halfPI;
+				lift = 1f;
+				break;
+			case SinShape.JFalling:
+				shift = 2 *_halfPI;
+				lift = 1f;
+				break;
+		}
+		
+		var result01 = lift + Mathf.Sin( shift + factor *_halfPI );
+		return Mathf.Lerp( min, max, result01 );
+		// return a + (1f - b) *result01;
+	}
+#endregion
+	
+	
+#region Private
+#endregion
+	
+	
+#region Temporary
+#endregion
+}
