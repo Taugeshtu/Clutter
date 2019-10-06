4c2440a1166a4d2c3ab32381319eeb0d4d8b2abf
Tihon <science.matter@gmail.com>
2019-10-06 20:30:16 +0300

    Watchdogs!

---
+++ General/WatchDog.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Debug = UnityEngine.Debug;
+
+public class WatchDog {
+	private Stopwatch m_watch;
+	
+	public string Name { get; private set; }
+	public long MS {
+		get {
+			return m_watch.ElapsedMilliseconds;
+		}
+	}
+	
+#region Implementation
+	public WatchDog( string name, bool autoStart = true ) {
+		m_watch = new Stopwatch();
+		Name = name;
+		
+		if( autoStart ) {
+			Start();
+		}
+	}
+#endregion
+	
+	
+#region Public
+	public void Start( bool reset = true ) {
+		if( reset ) {
+			m_watch.Reset();
+		}
+		m_watch.Start();
+	}
+	
+	public void Stop( bool autoReport = true ) {
+		m_watch.Stop();
+		
+		if( autoReport ) {
+			Log();
+		}
+	}
+	
+	public void Pause() {
+		Stop( false );
+	}
+	
+	public void Resume() {
+		Start( false );
+	}
+	
+	public string GetReport() {
+		return Name+": "+MS+" ms, "+m_watch.ElapsedTicks+" ticks";
+	}
+	
+	public void Log() {
+		Debug.LogError( GetReport() );
+	}
+#endregion
+	
+	
+#region Private
+#endregion
+	
+	
+#region Temporary
+#endregion
+}
