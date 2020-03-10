918beb121997072b02a82e6d3598670442e2be4a
Taugeshtu <tau.tihon@gmail.com>
2020-03-11 01:03:41 +0200

    Core-alized some extensions

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 public static class ListExtensions {
--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 public static class StringExtensions {
 	
@@ -8,7 +7,6 @@ public static class StringExtensions {
 		var removeStart = builder.Length - count;
 		builder.Remove( removeStart, count );
 	}
-	
 #endregion
 	
 	
