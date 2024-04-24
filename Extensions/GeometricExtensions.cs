c4bb9aab643dfc615228fbd330ae5132e04c3bd0
Tihon Gusev <tau.tihon@gmail.com>
2024-04-24 16:51:27 +0100

    Morton encoding for Vector3Int

--- Extensions/GeometricExtensions.cs
+++ Extensions/GeometricExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public static class GeometricExtensions {
 	
@@ -383,6 +384,20 @@ public static class GeometricExtensions {
 		rect.x -= (rectTransform.pivot.x *size.x);
 		rect.y -= ((1.0f - rectTransform.pivot.y) *size.y);
 		return rect;
+	}
+	
+	public static ulong MortonEncoded( this Vector3Int position ) {
+		UInt64 _splitBy3( UInt32 a ) {
+			UInt64 x = a & 0x1fffff; // we only look at the first 21 bits
+			x = (x | x << 32) & 0x1f00000000ffff; // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
+			x = (x | x << 16) & 0x1f0000ff0000ff; // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
+			x = (x | x << 8) & 0x100f00f00f00f00f; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
+			x = (x | x << 4) & 0x10c30c30c30c30c3; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
+			x = (x | x << 2) & 0x1249249249249249;
+			return x;
+		}
+		
+		return _splitBy3( (UInt32) position.x ) | _splitBy3( (UInt32) position.y ) << 1 | _splitBy3( (UInt32) position.z ) << 2;
 	}
 #endregion
 }
