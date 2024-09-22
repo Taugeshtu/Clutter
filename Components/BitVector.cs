06b52cfc2523e690ba1e4a5370c5e3f3c2af5cfc
Tihon Gusev <tau.tihon@gmail.com>
2024-09-23 00:08:36 +0100

    BitVector made debuggable

--- Components/BitVector.cs
+++ Components/BitVector.cs
@@ -1,18 +1,18 @@
 using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-public unsafe struct BitVector
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
+public struct BitVector
 {
-    private const int MaxStackAllocSize = 256; // 2048 bits
     private const int BitsPerLong = sizeof(ulong) * 8;
-
+    private readonly ulong[] _bits;
     private readonly int _length;
-    private readonly int _longCount;
-    private fixed ulong _stackBits[MaxStackAllocSize / sizeof(ulong)];
-    private ulong* _heapBits;
 
     public int Length => _length;
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebuggerDisplay => $"Length = {_length}, Bits = {ToString()}";
 
     public BitVector(int length)
     {
@@ -20,40 +20,8 @@ public unsafe struct BitVector
             throw new ArgumentOutOfRangeException(nameof(length), "Length must be positive.");
 
         _length = length;
-        _longCount = (length + BitsPerLong - 1) / BitsPerLong;
-        _heapBits = null;
-
-        if (_longCount > MaxStackAllocSize / sizeof(ulong))
-        {
-            _heapBits = (ulong*)Marshal.AllocHGlobal(_longCount * sizeof(ulong));
-        }
-
-        // Clear the memory
-        ulong* bits = GetBits();
-        for (int i = 0; i < _longCount; i++)
-        {
-            bits[i] = 0;
-        }
-    }
-
-    public void Dispose()
-    {
-        if (_heapBits != null)
-        {
-            Marshal.FreeHGlobal((IntPtr)_heapBits);
-            _heapBits = null;
-        }
-    }
-
-    private ulong* GetBits()
-    {
-        if (_heapBits != null)
-            return _heapBits;
-        
-        fixed (ulong* stackBits = _stackBits)
-        {
-            return stackBits;
-        }
+        int longCount = (length + BitsPerLong - 1) / BitsPerLong;
+        _bits = new ulong[longCount];
     }
 
     public bool this[int index]
@@ -63,19 +31,17 @@ public unsafe struct BitVector
             if ((uint)index >= (uint)_length)
                 throw new ArgumentOutOfRangeException(nameof(index));
 
-            ulong* bits = GetBits();
-            return (bits[index / BitsPerLong] & (1UL << (index % BitsPerLong))) != 0;
+            return (_bits[index / BitsPerLong] & (1UL << (index % BitsPerLong))) != 0;
         }
         set
         {
             if ((uint)index >= (uint)_length)
                 throw new ArgumentOutOfRangeException(nameof(index));
 
-            ulong* bits = GetBits();
             if (value)
-                bits[index / BitsPerLong] |= 1UL << (index % BitsPerLong);
+                _bits[index / BitsPerLong] |= 1UL << (index % BitsPerLong);
             else
-                bits[index / BitsPerLong] &= ~(1UL << (index % BitsPerLong));
+                _bits[index / BitsPerLong] &= ~(1UL << (index % BitsPerLong));
         }
     }
 
@@ -88,17 +54,26 @@ public unsafe struct BitVector
     public void SetAll(bool value)
     {
         ulong fill = value ? ulong.MaxValue : 0;
-        ulong* bits = GetBits();
-        for (int i = 0; i < _longCount; i++)
-            bits[i] = fill;
+        int fullLongCount = _length / BitsPerLong;
+        int remainingBits = _length % BitsPerLong;
+
+        // Fill all complete ulongs
+        for (int i = 0; i < fullLongCount; i++)
+            _bits[i] = fill;
+
+        // Handle the last, potentially partial ulong
+        if (remainingBits > 0)
+        {
+            ulong mask = (1UL << remainingBits) - 1;
+            _bits[fullLongCount] = value ? mask : 0;
+        }
     }
 
     public int PopCount()
     {
         int count = 0;
-        ulong* bits = GetBits();
-        for (int i = 0; i < _longCount; i++)
-            count += PopCount(bits[i]);
+        for (int i = 0; i < _bits.Length; i++)
+            count += PopCount(_bits[i]);
         return count;
     }
 
@@ -116,12 +91,8 @@ public unsafe struct BitVector
             throw new ArgumentException("Vectors must have the same length.");
 
         var result = new BitVector(left._length);
-        ulong* leftBits = left.GetBits();
-        ulong* rightBits = right.GetBits();
-        ulong* resultBits = result.GetBits();
-
-        for (int i = 0; i < left._longCount; i++)
-            resultBits[i] = leftBits[i] & rightBits[i];
+        for (int i = 0; i < left._bits.Length; i++)
+            result._bits[i] = left._bits[i] & right._bits[i];
 
         return result;
     }
@@ -132,12 +103,8 @@ public unsafe struct BitVector
             throw new ArgumentException("Vectors must have the same length.");
 
         var result = new BitVector(left._length);
-        ulong* leftBits = left.GetBits();
-        ulong* rightBits = right.GetBits();
-        ulong* resultBits = result.GetBits();
-
-        for (int i = 0; i < left._longCount; i++)
-            resultBits[i] = leftBits[i] | rightBits[i];
+        for (int i = 0; i < left._bits.Length; i++)
+            result._bits[i] = left._bits[i] | right._bits[i];
 
         return result;
     }
@@ -148,12 +115,8 @@ public unsafe struct BitVector
             throw new ArgumentException("Vectors must have the same length.");
 
         var result = new BitVector(left._length);
-        ulong* leftBits = left.GetBits();
-        ulong* rightBits = right.GetBits();
-        ulong* resultBits = result.GetBits();
-
-        for (int i = 0; i < left._longCount; i++)
-            resultBits[i] = leftBits[i] ^ rightBits[i];
+        for (int i = 0; i < left._bits.Length; i++)
+            result._bits[i] = left._bits[i] ^ right._bits[i];
 
         return result;
     }
@@ -161,21 +124,17 @@ public unsafe struct BitVector
     public static BitVector operator ~(BitVector vector)
     {
         var result = new BitVector(vector._length);
-        ulong* vectorBits = vector.GetBits();
-        ulong* resultBits = result.GetBits();
-
-        for (int i = 0; i < vector._longCount; i++)
-            resultBits[i] = ~vectorBits[i];
+        for (int i = 0; i < vector._bits.Length; i++)
+            result._bits[i] = ~vector._bits[i];
 
         return result;
     }
     
     public override string ToString()
     {
-        int spacesCount = (_length - 1) / 4; // Pre-calculate the number of spaces needed
+        int spacesCount = (_length - 1) / 4;
         int totalLength = _length + spacesCount;
         char[] result = new char[totalLength];
-        ulong* bits = GetBits();
 
         for (int i = 0, j = totalLength - 1; i < _length; i++)
         {
@@ -185,13 +144,12 @@ public unsafe struct BitVector
                 j--;
             }
 
-            int longIndex = i / BitsPerLong;
-            int bitIndex = i % BitsPerLong;
-            bool isSet = (bits[longIndex] & (1UL << bitIndex)) != 0;
-            result[j] = isSet ? 'X' : '_';
+            result[j] = this[i] ? 'X' : '_';
             j--;
         }
 
         return new string(result);
     }
+    
+    public string Print => ToString();
 }
